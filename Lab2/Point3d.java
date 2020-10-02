791afdd272f331a134b7dfc124ef5d68649fad33
Snool <killwerito@gmail.com>
2020-10-02 20:28:36 +0300

    Add files via upload
    Update

--- Lab2/Point3d.java
+++ Lab2/Point3d.java
@@ -1,12 +1,12 @@
-public class Point3d {
+public class Point3d extends Point2d{
     // Class description
-    // Coordinates x, y, z
-    private double xCoord, yCoord, zCoord;
+    // Coordinates z
+    private double zCoord;
 
     // Class contructor
     public Point3d(double x, double y, double z){
-        xCoord = x;
-        yCoord = y;
+        super.setX(x);
+        super.setY(y);
         zCoord = z;
     }
 
@@ -15,29 +15,9 @@ public class Point3d {
         this (0, 0, 0);
     }
 
-    // Get x coordinate
-    public double getX() {
-        return xCoord;
-    }
-
-    // Get y coordinate
-    public double getY() {
-        return yCoord;
-    }
-
     // Get z coordinate
     public double getZ() {
         return zCoord;
-    }
-
-    // Set x coordinate
-    public void setX(double x) {
-        xCoord = x;
-    }
-
-    // Set y coordinate
-    public void setY(double y) {
-        yCoord = y;
     }
 
     // Set z coordinate
@@ -50,6 +30,8 @@ public class Point3d {
         double objx = objPoint3d.getX();
         double objy = objPoint3d.getY();
         double objz = objPoint3d.getZ();
+        double xCoord = this.getX();
+        double yCoord = this.getY();
         if ( (xCoord == objx) && (yCoord == objy) && (zCoord == objz)){
             return true;
         }
@@ -61,6 +43,8 @@ public class Point3d {
         double objx = objPoint3d.getX();
         double objy = objPoint3d.getY();
         double objz = objPoint3d.getZ();
+        double xCoord = this.getX();
+        double yCoord = this.getY();
         return Math.sqrt(Math.pow(objx - xCoord, 2) + Math.pow(objy - yCoord, 2) + Math.pow(objz - zCoord, 2));
     }
 }
